bf060ab0c5faf861f2943090d3b888def4931ef6
MuLip-Stargazer (Admin) <goelshivam1210@gmail.com>
2022-12-09 14:30:56 -0500

    Finished ProblemGenSimple class

---
+++ pddl/simple/generated_problem.pddl
@@ -0,0 +1,55 @@
+(define (problem simple_problem) (:domain simple_domain)
+(:objects
+    f0-0f - location
+    f0-1f - location
+    f0-2f - location
+    f1-2f - location
+    f2-2f - location
+    f3-2f - location
+    f4-2f - location
+    f5-2f - location
+    f5-3f - location
+    f5-4f - location
+)
+
+(:init
+    (move_up)
+    (move_down)
+    (move_right)
+    (move_left)
+    (at hello)
+
+    (below f0-1f f0-0f)
+    (above f0-0f f0-1f)
+
+    (below f0-2f f0-1f)
+    (above f0-1f f0-2f)
+
+    (right f1-2f f0-2f)
+    (left f0-2f f1-2f)
+
+    (right f2-2f f1-2f)
+    (left f1-2f f2-2f)
+
+    (right f3-2f f2-2f)
+    (left f2-2f f3-2f)
+
+    (right f4-2f f3-2f)
+    (left f3-2f f4-2f)
+
+    (right f5-2f f4-2f)
+    (left f4-2f f5-2f)
+
+    (below f5-3f f5-2f)
+    (above f5-2f f5-3f)
+
+    (below f5-4f f5-3f)
+    (above f5-3f f5-4f)
+
+    (is-goal hello)
+        )
+
+(:goal (and
+    (at hello)
+    ))
+)
--- python/problem_gen.py
+++ python/problem_gen.py
@@ -6,17 +6,61 @@ class ProblemGenSimple(object):
     '''
 
     def __init__(self, 
-                problem_basis_path='../pddl/simple/problem_basis.pddl',
-                problem_file_path='../pddl/simple/generated_problem.pddl'):
+                problem_file_path='/../pddl/simple/generated_problem.pddl'):
         
         '''
         Initializes problem generator for simple domain.
 
-        problem_file_path: relative path to problem file to base new problem files from.
+        problem_file_path: relative path to the newly created problem files.
         returns: none.
         '''
 
-        self.problem_basis_path = os.path.dirname(__file__) + problem_basis_path
+        self.problem_file_path = os.path.dirname(__file__) + problem_file_path
+
+        self.problem = \
+        "(define (problem simple_problem) (:domain simple_domain)\n\
+(:objects\n\
+    f0-0f - location\n\
+    f0-1f - location\n\
+    f0-2f - location\n\
+    f1-2f - location\n\
+    f2-2f - location\n\
+    f3-2f - location\n\
+    f4-2f - location\n\
+    f5-2f - location\n\
+    f5-3f - location\n\
+    f5-4f - location\n\
+)\n\n\
+(:init\n\
+    (move_up)\n\
+    (move_down)\n\
+    (move_right)\n\
+    (move_left)\n\
+    (at %s)\n\n\
+    (below f0-1f f0-0f)\n\
+    (above f0-0f f0-1f)\n\n\
+    (below f0-2f f0-1f)\n\
+    (above f0-1f f0-2f)\n\n\
+    (right f1-2f f0-2f)\n\
+    (left f0-2f f1-2f)\n\n\
+    (right f2-2f f1-2f)\n\
+    (left f1-2f f2-2f)\n\n\
+    (right f3-2f f2-2f)\n\
+    (left f2-2f f3-2f)\n\n\
+    (right f4-2f f3-2f)\n\
+    (left f3-2f f4-2f)\n\n\
+    (right f5-2f f4-2f)\n\
+    (left f4-2f f5-2f)\n\n\
+    (below f5-3f f5-2f)\n\
+    (above f5-2f f5-3f)\n\n\
+    (below f5-4f f5-3f)\n\
+    (above f5-3f f5-4f)\n\n\
+    (is-goal %s)\n\
+        )\n\n\
+(:goal (and\n\
+    (at %s)\n\
+    ))\n\
+)"
 
     def generate(self,
                 at,
@@ -24,8 +68,27 @@ class ProblemGenSimple(object):
         '''
         Generates a problem file given the agent's position and goal position.
 
+        at: str describing agents position
+        goal: str describing goal
         returns: none.
         '''
 
-        problem_file = open(self.problem_basis_path, "r")
-        problem_file = open(self. )
+        # Check types for input
+        if type(at) != str and type(goal) != str:
+            raise TypeError("Parameters 'at' and 'goal' must be 'str'.")
+
+
+        # Write formatted problem
+        problem_str = self.problem % (at, goal, goal)
+
+        # Open file
+        problem_file = open(self.problem_file_path, "w")
+
+        # Write to the file
+        problem_file.write(problem_str)
+
+
+if __name__ == '__main__':
+
+    gen = ProblemGenSimple()
+    gen.generate("test", "tes")
