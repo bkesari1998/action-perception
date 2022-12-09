79823a98546656452c512d436b7412e7346adae7
Bharat Kesari <bkesari1998@gmail.com>
2022-12-08 20:51:32 -0500

    Implemented __init__ and create_plan methods

--- python/planner.py
+++ python/planner.py
@@ -1,8 +1,31 @@
 from pyperplan.search.sat import sat_solve
+from pyperplan.planner import _parse, _ground
 
 class Planner(object):
     '''
     Wrapper for pyperplan sat_solver planner.
     '''
 
-    def __init__(self, domain):
+    def __init__(self, 
+                domain):
+        '''
+        Initializes planner class.
+
+        domain: path to pddl domain file.
+        '''
+
+        self.domain = domain
+        self.task = None
+
+    def create_plan(self,
+                    problem):
+        '''
+        Creates task for solver.
+
+        problem: path to pddl problem file.
+        returns: plan created by sat solver
+        '''
+
+        problem = _parse(self.domain, problem)
+        task = _ground(problem)
+        return sat_solve(task)
