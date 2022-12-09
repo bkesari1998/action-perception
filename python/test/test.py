56f568510d7ac2cc59b553bc8f707e4377dc964b
Bharat Kesari <bkesari1998@gmail.com>
2022-12-08 20:23:03 -0500

    added paths to test.py

--- python/test/test.py
+++ python/test/test.py
@@ -1,10 +1,15 @@
+
+import sys
+sys.path.append('../../../pddlgym')
+sys.path.append('/Users/bharatkesari/Documents/tufts/academic/cs_137/project')
+
 import pddlgym
 from pddlgym_planners.fd import FD
 import time
 
 import matplotlib; matplotlib.use('agg')
 import imageio
-
+ 
 # See `pddl/sokoban.pddl` and `pddl/sokoban/problem3.pddl`.
 env = pddlgym.make("PDDLEnvSimple-v0")
 # env.fix_problem_index(2)
@@ -21,7 +26,7 @@ render_and_save(env, i=0)
 for i, act in enumerate(plan):
     # print("Obs:", obs)
     print("Act:", act)
-    obs, reward, done, info = env.step(act)
+    obs, reward, done, info = env.step(act[0])
     render_and_save(env, i=i + 1)
     
 print("Final obs, reward, done:", obs, reward, done)
