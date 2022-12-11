1522cefc2258a00e3c13b18aea256798f472ab66
goelshivam1210 <goelshivam1210@gmail.com>
2022-12-11 15:22:59 -0500

    added pipeline

--- python/run.py
+++ python/run.py
@@ -30,43 +30,54 @@ class Experiment(object):
         self.model = CNN(num_locations=self.problem_generator.num_locations)
         self.satsolver = None
 
-    def run(self):
+    def run(self, reset=False):
         '''
         Runs the experiment.
         returns: nothing.
         '''
         # Get first observation
-        obs, info = self.environment.reset()
+
+        obs = None
+        
+        if not reset:
+            obs = self.environment.rendering_to_obs(self.environment.render())
+            self.environment.timestep = 0
+        else:
+            obs, _ = self.environment.reset()
+        
         # Initialize the model
         prediction = self.model.forward(obs)
         agent_loc, goal_loc = self.get_locations(prediction)
         # Generate the problem file for planner
         self.problem_generator.generate(agent_loc, goal_loc)
         prob_path = '../pddl/simple/generated_problem.pddl'
-
-        self.satsolver = SATSolverModel(domain_file=self.domain_path, problem_file=prob_path)
-
         plan = self.planner.create_plan(prob_path)
+        # Initialize the SAT solver
+        self.satsolver = SATSolverModel(domain_file=self.domain_path, problem_file=prob_path, plan_horizon=len(plan))
 
 
+        last_obs, last_act = None, None
         for i, act in enumerate(plan):
             print(act)
             obs, timestep, done, info = self.environment.step(act)
+            last_obs = obs
+            last_act = act
             if info['result'] != 'success':
-                # start reasoning now for the loss function
-                self.satsolver.report_action_result(action=act.predicate.name, iter=i, success=False)
-                reasoned_samples = self.satsolver.get_start_rates(num_samples=MONTE_CARLO_SAMPLES)
-                self.model.train(x = obs, y = reasoned_samples)
                 break
-            elif done:
-                # todo also maybe do processing here
-                break
+        # now also update the model for the last action
+        self.satsolver.report_action_result(action=last_act.predicate.name, iter=i, success=False)
+        reasoned_samples = self.satsolver.get_start_rates(num_samples=MONTE_CARLO_SAMPLES)
+        self.model.train(x = last_obs, y = reasoned_samples)
+        return done
 
     def load_model():
         pass
 
-    def train_model():
-        pass
+    def train_model(self, num_eps):
+        reset = True
+        for episode in range(num_eps):
+            reset = self.run(reset)
+
 
     def get_locations(self, prediction):
         '''
@@ -87,205 +98,3 @@ if __name__ == '__main__':
 
     exp_1 = Experiment()
     exp_1.run()
-    # send to print results
-    # print results
-
-    # # Initialize environment
-    # env = Environment()
-    # # initialize the problem generator
-    # gen = ProblemGenSimple()
-    # # get first observation
-    # obs, info = env.reset()
-    # # initialize the model
-    # network = CNN(info['num_of_locs'])
-    # prediction = network.forward(obs)
-    # agent_loc, goal_loc = get_locations(prediction)
-    # # generate the problem file for planner
-    # gen.generate(agent_loc, goal_loc)
-    # prob_path = '/../pddl/simple/generated_problem.pddl'
-    # domain_path = '/../pddl/simple.pddl'
-
-    # planner = Planner(domain_path)
-    # plan = planner.create_plan(prob_path)
-
-    # for act in plan:
-    #     print(act)
-    #     obs,info = env.step(act)
-    #     if info['result'] != 'success':
-    #         break
-    
-
-
-
-
-
-
-    # # send the image to the model.
-    # # get the action from the model.
-    # # step the environment with the action.
-    # # repeat.
-
-
-
-
-
-
-    # # Load model
-    # model = load_model()
-
-    # # Train model
-    # model = train_model()
-
-
-    # # Initialize planner
-    # domain_path = '../pddl/simple.pddl'
-    # planner = Planner(domain_path)
-
-    # # Initialize problem generator
-    # gen = ProblemGenSimple()
-
-    # # Generate problem
-    # at = 'f0-0f'
-    # goal = 'f5-4f'
-    # gen.generate(at, goal)
-    # prob_path = '../pddl/simple/generated_problem.pddl'
-
-    # # Create plan
-    # plan = planner.create_plan(prob_path)
-
-    # # Execute plan
-    # for act in plan:
-    #     print(act)
-    #     env.step(act)
-
-    # # Save plan
-    # # ...
-
-    # # Save model
-    # # ...
-
-    # # Save environment
-    # # ...
-
-    # # Save planner
-    # # ...
-
-    # # Save problem generator
-    # # ...
-
-    # # Save experiment
-    # # ...
-
-    # # Save results
-    # # ...
-
-    # # Save data
-    # # ...
-
-    # # Save logs
-    # # ...
-
-    # # Save images
-    # # ...
-
-    # # Save videos
-    # # ...
-
-    # # Save graphs
-    # # ...
-
-    # # Save plots
-    # # ...
-
-    # # Save figures
-    # # ...
-
-    # # Save tables
-    # # ...
-
-    # # Save statistics
-    # # ...
-
-    # # Save metrics
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
-
-    # # Save scores
-    # # ...
