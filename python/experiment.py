bbffd75bb19ed93a66e6bb0d7b2729e63696d8e1
Bharat Kesari <bkesari1998@gmail.com>
2022-12-14 11:25:13 -0500

    Added metric to count successful action rate

--- python/experiment.py
+++ python/experiment.py
@@ -34,6 +34,19 @@ class Experiment(object):
         self.model = CNN(num_locations=self.problem_generator.num_locations)
         self.goal = 'f5-4f'
 
+        self.loc_dict = {
+            "f0-0f": 0,
+            "f0-1f": 1,
+            "f0-2f": 2,
+            "f1-2f": 3,
+            "f2-2f": 4,
+            "f3-2f": 5,
+            "f4-2f": 6,
+            "f5-2f": 7,
+            "f5-3f": 8,
+            "f5-4f": 9,
+        }
+
     def run(self, epi):
         '''
         Runs the experiment.
@@ -44,6 +57,11 @@ class Experiment(object):
 
         # Get first observation
         obs, _ = self.environment.reset()
+
+        # Ground truth init location
+        gt_loc = self.environment.location
+        optimal_plan_length = self.loc_dict[self.goal] - self.loc_dict[gt_loc]
+
         
         # Initialize the model
         prediction = self.model.forward(obs)
@@ -68,6 +86,8 @@ class Experiment(object):
             return True, None, None
 
         Done = None
+        num_successful_actions = 0
+
         for i, act in enumerate(plan):
             _, _, done, info = self.environment.step(act)
 
@@ -79,6 +99,9 @@ class Experiment(object):
                 "successful" if info['result'] else "failed"
             )
 
+            if info['result']:
+                num_successful_actions += 1
+
             if not info['result']:
                 # start reasoning now for the loss function
                 # reasoned_samples = self.satsolver.get_start_rates(num_samples=MONTE_CARLO_SAMPLES)
@@ -86,19 +109,21 @@ class Experiment(object):
                 break
             elif done:
                 break
+        
+        successful_actions_vs_opt_plan_len = num_successful_actions / optimal_plan_length
 
         reasoned_samples = self.satsolver.get_start_rates(num_samples=MONTE_CARLO_SAMPLES)
-        print("Reasoned Samples: ", reasoned_samples.squeeze())
+        print("Reasoned Samples: ", reasoned_samples.squeeze().round(2))
 
         # Get probabilities of predicted locations
         model_probs = self.model.get_probability(prediction)
-        print("Model Probabilities: ", model_probs.detach().numpy().squeeze())
+        print("Model Probabilities: ", model_probs.detach().numpy().squeeze().round(2))
 
         # Train over the samples for 100 iterations
         for i in range(100):
             loss = self.model.train(x = obs, y = reasoned_samples)
 
-        return done, loss
+        return done, loss, successful_actions_vs_opt_plan_len
 
     def get_locations(self, prediction, exclude_agent_loc=[]):
         '''
@@ -131,9 +156,13 @@ if __name__ == '__main__':
 
     exp_1 = Experiment()
     loss_history = []
+    action_success_rate = []
+    batch_action_success_rate = []
+
     for i in range(1000):
-        done, loss = exp_1.run(i)
+        done, loss, rate = exp_1.run(i)
         loss_history.append(loss.detach().numpy())
+        action_success_rate.append(rate)
 
         if done:
             print("Agent reached goal!")
@@ -144,9 +173,13 @@ if __name__ == '__main__':
             print("Saving loss history...")
             np.save('loss_history.npy', np.array(loss_history))
         
-        if i % 1 == 0:
+        if i % 10 == 0:
             print("Training Loss: ", loss.detach().numpy())
+            mean_action_success_rate = np.mean(action_success_rate)
+            print("Action Success Rate: ", mean_action_success_rate)
+            action_success_rate = []
+            batch_action_success_rate.append(mean_action_success_rate)
         
         print()
         print()
-        
+
